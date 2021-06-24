6fd791387f083585bd19e0adb0c727a357d32ec0
BhuvanaPari <bhuvana1837@gmail.com>
2021-06-24 18:24:21 +0530

    Orbits plotted - rescaling needed

--- BinarySystemSimulator/trajectory.py
+++ BinarySystemSimulator/trajectory.py
@@ -1,76 +1,94 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-G = 6.67e-11
-m_solar = 2e30
-au = 1.496e11
+"""
+Updates to be made:
+    Trial runs with actual values of constants, and better plot visualization
+    Generalize initialization conditions, with correct expressions
+    Need to incorporate the orbital time period of the binary system
+"""
 
+#need to be replaced with actual values and scaled accordinglt for the right plots!!!
+G=1.0
+m_solar=1.0
+au=10.0
+
+#G=6.67e-11
+#m_solar=2e30
+#au=1.5e11
 
 class BinarySystem:
-    """[summary]
-    pos1, pos2: positions in speherical coordinates (numpy array of three numbers)
-    omega: orbital frequency
-    e: eccentricity
-    a: semi_major axis length
-    acc1: acceleration of mass 1
-    acc2: acceleration of mass 2
-    vel1: velocity of mass 1
-    vel2 : velocity of mass 2
-
     """
+    Simulates a Binary star system. Both the stars are considered as point masses. 
+    Only the presence of gravitational force is considered. 
+    NEED TO FIGURE OUT THE SCALING!!!
 
-    def __init__(self, m1, m2, a, omega, e, dt=1) -> None:
-        self.m1 = m1 * m_solar
-        self.m2 = m2 * m_solar
-        # self.omega = omega
-        self.e = e
-        self.p1 = np.array([-0.5 * au, 0.0 * au])
-        self.p2 = np.array([0.5 * au, 0.0 * au])
-        self.a = a
-        self.dt = 1
+    Args:
+        m1 (float): stellar mass 1
+        m2 (float): stellar mass 2
+        dt (float): timestep of the simulation (in seconds)
+    
+    """
+    def __init__(self,m1,m2,dt=0.01):
+        """
+        Function run to initialize the BinarySystem class. The initial positions are taken along the x-axis.
+        Initial velocities taken perpendicular to the line joining the centers.
 
-        self.com = (self.m1 * self.p1 + self.m2 * self.p2) / (self.m1 + self.m2)
-        self.pos1 = self.p1 - self.com
-        self.pos2 = self.p2 - self.com
+        """
+        self.m1=m1
+        self.m2=m2
+        self.dt=dt
 
-        # v1_init= -G*self.m2 * np.linalg.norm(self.pos1)*(1+self.e) / (np.linalg.norm(self.pos1 - self.pos2)**2)
-        # v2_init= G*self.m1 * np.linalg.norm(self.pos2)*(1+self.e) / (np.linalg.norm(self.pos1 - self.pos2)**2)
-        self.v1 = np.array([1, -0.01]) * au
-        self.v2 = np.array([0.01, 0.01]) * au
+        self.pos1=np.array([-1*au,0])
+        self.pos2=np.array([1*au,0])
+        self.vel1=np.array([0,0.3])
+        self.vel2=np.array([0,-0.3])
+        
+        F=self.force()
+        self.acc1=F/self.m1
+        self.acc2=-F/self.m2
 
-    @property
     def force(self):
-        r = self.pos2 - self.pos1
-        r_norm = np.linalg.norm(r)
-        r_unit = r / r_norm
-        f = (G * self.m1 * self.m2 / r_norm ** 2) * r_unit
-        return f
+        """
+        Function returns the gravitational force on MASS1 due to mass2. 
+        The force on MASS2 due to mass1 would be the negative of this value
+        """
+        r=self.pos1-self.pos2
+        r_mag=np.linalg.norm(r)
+        r_unit=r/r_mag
+        F= -G*self.m1*self.m2/(r_mag**2) * r_unit
+        return F
 
     def update_position(self):
-        self.acc1 = self.force / self.m1
-        self.acc2 = -self.force / self.m2
-        self.pos1 += self.acc1 * (self.dt ** 2) + self.v1 * self.dt
-        self.pos2 += self.acc2 * (self.dt ** 2) + self.v2 * self.dt
-        self.v1 += self.acc1 * self.dt
-        self.v2 += self.acc2 * self.dt
-        return self.pos1, self.pos2, self.com
+        """
+        Function updates the positions of mass1 and mass2 at each time step dt.
+        It calls the force() function to update the acceleration, and hence velocity as well.
+        """
+        self.pos1+=self.vel1*self.dt
+        self.pos2+=self.vel2*self.dt
+        self.vel1+=self.acc1*self.dt
+        self.vel2+=self.acc2*self.dt
+        F=self.force()
+        self.acc1=F/self.m1
+        self.acc2=-F/self.m2
 
 
-if __name__ == "__main__":
-    binary = BinarySystem(1, 1, 0, 0, 2)
-    star1_loc = np.zeros((100, 2))
-    star2_loc = np.zeros((100, 2))
-    c = np.zeros(2)
-    for i in range(100):
-        p1, p2, c = binary.update_position()
-        star1_loc[i, :] = p1
-        star2_loc[i, :] = p2
-        # print(f"{p1=};{p2=}")
 
-    plt.figure(1)
-    plt.plot(star1_loc[:, 0], star1_loc[:, 1], "r.", label="mass1")
-    plt.plot(star2_loc[:, 0], star2_loc[:, 0], "b.", label="mass2")
-    plt.plot(c[0], c[1], "g*", label="center of mass")
-    plt.legend()
-    plt.savefig("traj.png")
-    plt.show()
+m1=5*m_solar
+m2=5*m_solar
+bin_sys=BinarySystem(m1,m2)
+
+n=int(2e4)
+loc_mass1=np.zeros((n,2))
+loc_mass2=np.zeros((n,2))
+for i in range(n):
+    bin_sys.update_position()
+    loc_mass1[i,:]=bin_sys.pos1
+    loc_mass2[i,:]=bin_sys.pos2
+
+plt.plot(loc_mass1[:,0],loc_mass1[:,1],label='mass 1')
+plt.plot(loc_mass2[:,0],loc_mass2[:,1],label='mass 2')
+plt.legend()
+plt.savefig("binary_orbits.png")
+
+
