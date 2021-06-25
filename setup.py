9fdac50e0d3ecfb9d246b990fe4be4cb67ce9b24
Amarjeet Kumar <masteramarjeetkumar@gmail.com>
2021-06-25 19:59:50 +0530

    Upload code to testpypi

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name="Binary_System_Simulator", version="0.1.0", packages=find_packages())
+setup(
+    name="SpectroBinary",
+    version="1.0.0",
+    author="Amarjeet Kumar, Bhuvanambiga Pari",
+    author_email="masteramarjeetkumar@gmail.com",
+    license="MIT",
+    url="https://github.com/MASTERAMARJEET/code-astro-2021",
+    packages=find_packages(),
+    install_requires=["numpy", "matplotlib"],
+)
 
